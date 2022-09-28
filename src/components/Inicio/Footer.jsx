cddf8c04956d53c29a440780f5097831733eebb0
Alejo Gomez <alejandro.gomezq97@gmail.com>
2022-09-28 17:45:10 -0500

    BREAKING CHANGE: upload footer

--- src/components/Inicio/Footer.jsx
+++ src/components/Inicio/Footer.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import '../../styles/Footer.css';
 import logotitulo from '../../asssets/icons/logotitulo.png';
 import facebook from '../../asssets/icons/facebook.svg';
@@ -8,21 +10,23 @@ import twitter from '../../asssets/icons/twitter.svg';
 export const Footer = () => {
 
     return (
-        <footer className='footer w-100'>
-            <img className='logoFooter' src={logotitulo} alt="" />
-            <div className='footerI'>
-                <a href="">Informacion</a>
-                <a href="">Ayuda</a>
-                <a href="">Politicas de privacidad</a>
+        <footer className='footer container-fluid w-100 h-auto'>
+            <div className='footer-container d-flex justify-content-between align-items-center p-4'>
+                <Link to="/"><img className='footer-img' src={logotitulo} alt="logo" /></Link>
+                <div className='footer-menu'>
+                    <a href="" className='footer-menu__item m-3'>Informacion</a>
+                    <a href="" className='footer-menu__item m-3'>Ayuda</a>
+                    <a href="" className='footer-menu__item m-3'>Politicas de privacidad</a>
+                </div>
+                <div className='social-media-container h-auto'>
+                    <a href=""> <img className='social-media-logo' src={facebook} alt="facebook-logo" /></a>
+                    <a href=""> <img className='social-media-logo' src={instagram} alt="instagram-logo" /></a>
+                    <a href=""> <img className='social-media-logo' src={twitter} alt="twitter-logo" /></a>
+                    <a href=""> <img className='social-media-logo' src={red} alt="red-logo" /> </a>
+                </div>
             </div>
-            <hr/>
-            <div>
-                <a href=""> <img className='logos' src={facebook} alt="" /></a>
-                <a href=""> <img className='logos'  src={instagram} alt="" /></a>
-                <a href=""> <img className='logos' src={twitter} alt="" /></a>
-                <a href=""> <img className='logos' src={red} alt="" /> </a>
-            </div>
-            <div><p className="m-0">© 2022 Mís Artesanías. All rights reserved</p></div>
+            <hr />
+            <p>© 2022 Mís Artesanías. All rights reserved</p>
         </footer>
     )
 }
